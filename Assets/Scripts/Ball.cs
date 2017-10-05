e1d597d803cdc6602cb2c4542403c48d43a8ed1e
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2017-10-05 22:53:27 +0300

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+public class Ball : MonoBehaviour
+{
+    public float velocityForce = 200;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.forward * velocityForce;
+        GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Ball Ball;
+
+    private Vector3 offset;
+    
+    // Use this for initialization
+    void Start()
+    {
+        offset = new Vector3(0, 54, -160);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ball.gameObject.transform.position.z <= 1800)
+        {
+            transform.position = Ball.gameObject.transform.position + offset;
+        }
+    }
+}
