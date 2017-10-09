a76a5e4304cd73f203b94aea4a669e801706bb9e
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2017-10-09 17:56:11 +0300

    Added Pin script

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pin : MonoBehaviour {
+public class Pin : MonoBehaviour
+{
+    public float StandingThreshold;
+    
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public bool IsStanding()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+
+        float tiltX = Mathf.Abs(rotation.x);
+        float tiltZ = Mathf.Abs(rotation.z);
+
+        return transform.rotation.y >= StandingThreshold;
+    }
 }
