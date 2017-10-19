76591a2f57e40881a58d1aba41560299e14b9721
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2017-10-19 09:06:41 +0300

    Added ActionMaster tests

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+[TestFixture]
+public class ActionMasterTest
+{
+    ActionMaster actionMaster = new ActionMaster();
+    
+    [Test]
+    public void T01OneStrikeReturnsEndTurn()
+    {
+        Assert.AreEqual(ActionMaster.Action.END_TURN, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T02Bowl8ReturnsTidy()
+    {
+        Assert.AreEqual(ActionMaster.Action.TIDY, actionMaster.Bowl(8));
+    }
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster
+{
+    private int[] bowls = new int[21];
+    private int bowl = 1;
+
+    public Action Bowl(int pins)
+    {
+        if (pins < 0 || pins > 10)
+        {
+            throw new UnityException("Invalid pins number: must be in range [0; 10]");
+        }
+        
+        switch (pins)
+        {
+            case 10:
+                return Action.END_TURN;
+            default:
+                throw new UnityException("Not sure what action to return");
+        }
+    }
+    
+    public enum Action
+    {
+        TIDY,
+        RESET,
+        END_TURN,
+        END_GAME
+    }
+}
