eab294607b3cf430b55d8ffa75b9ae605b5681a9
Arthur Dvurechenskiyi <Dvurechenskiyi@cdc.ru>
2017-10-20 18:31:11 +0300

    Finished tests for ActionMaster

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEditor;
 using UnityEngine.TestTools;
 using NUnit.Framework;
@@ -7,7 +8,13 @@ using System.Collections;
 [TestFixture]
 public class ActionMasterTest
 {
-    ActionMaster actionMaster = new ActionMaster();
+    private ActionMaster actionMaster;
+
+    [SetUp]
+    public void Setup()
+    {
+        actionMaster = new ActionMaster();
+    }
     
     [Test]
     public void T01OneStrikeReturnsEndTurn()
@@ -16,8 +23,100 @@ public class ActionMasterTest
     }
 
     [Test]
-    public void T02Bowl8ReturnsTidy()
+    public void T02Bowl0To9ReturnsTidy()
     {
-        Assert.AreEqual(ActionMaster.Action.TIDY, actionMaster.Bowl(8));
+        actionMaster = new ActionMaster();
+        Assert.AreEqual(ActionMaster.Action.TIDY, actionMaster.Bowl(0));
+        
+        actionMaster = new ActionMaster();
+        Assert.AreEqual(ActionMaster.Action.TIDY, actionMaster.Bowl(5));
+        
+        actionMaster = new ActionMaster();
+        Assert.AreEqual(ActionMaster.Action.TIDY, actionMaster.Bowl(9));
+    }
+    
+    [Test]
+    public void T03BowlsSpareReturnsEndTurn()
+    {
+        actionMaster.Bowl(1);
+        Assert.AreEqual(ActionMaster.Action.END_TURN, actionMaster.Bowl(9));
+
+        // if player bowls first for 0 and second for 10 - it is also spare
+        actionMaster = new ActionMaster();
+        actionMaster.Bowl(0);
+        Assert.AreEqual(ActionMaster.Action.END_TURN, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T04Bowls29ThrowsArgumentException()
+    {
+        // in case if 2nd call actionMaster.Bowl() with number of pins > remained on field
+        actionMaster.Bowl(2);
+        Assert.Throws<ArgumentException>(() => actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void T05BowlLess0OrMore10ThrowsArgumentException()
+    {
+        actionMaster = new ActionMaster();
+        Assert.Throws<ArgumentException>(() => actionMaster.Bowl(11));
+        
+        actionMaster = new ActionMaster();
+        Assert.Throws<ArgumentException>(() => actionMaster.Bowl(-1));
+    }
+
+    [Test]
+    public void T06BowlsTwiceReturnEndTurn()
+    {
+        actionMaster.Bowl(3);
+        Assert.AreEqual(ActionMaster.Action.END_TURN, actionMaster.Bowl(5));
+    }
+
+    [Test]
+    public void T07BowlSpare10FrameReturnReset()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(3);
+        }
+
+        actionMaster.Bowl(3);
+        Assert.AreEqual(ActionMaster.Action.RESET, actionMaster.Bowl(7));
+    }
+
+    [Test]
+    public void T08Strike10FrameReturnReset()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            actionMaster.Bowl(3);
+            actionMaster.Bowl(3);
+        }
+        
+        Assert.AreEqual(ActionMaster.Action.RESET, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T09BonusBowlReturnEndGame()
+    {
+        // case bonus bowls strikes 0 pins
+        for (int i = 0; i < 10; i++)
+        {
+            actionMaster.Bowl(5);
+            actionMaster.Bowl(5);
+        }
+        
+        Assert.AreEqual(ActionMaster.Action.END_GAME, actionMaster.Bowl(0));
+
+        // case bonus bowl strikes 10 pins
+        actionMaster = new ActionMaster();
+        for (int i = 0; i < 10; i++)
+        {
+            actionMaster.Bowl(5);
+            actionMaster.Bowl(5);
+        }
+        
+        Assert.AreEqual(ActionMaster.Action.END_GAME, actionMaster.Bowl(10));
     }
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,23 +7,82 @@ public class ActionMaster
 {
     private int[] bowls = new int[21];
     private int bowl = 1;
+    private int lastBowlPinsCount = 0;
 
+    // todo refactor
     public Action Bowl(int pins)
     {
         if (pins < 0 || pins > 10)
         {
-            throw new UnityException("Invalid pins number: must be in range [0; 10]");
+            throw new ArgumentException("Invalid pins number. For first bowl must be in range [0; 10]");
         }
-        
-        switch (pins)
+
+        int pinsInGame = 10 - lastBowlPinsCount;
+
+        if (lastBowlPinsCount != 0 && pinsInGame - pins < 0)
         {
-            case 10:
-                return Action.END_TURN;
-            default:
-                throw new UnityException("Not sure what action to return");
+            throw new ArgumentException(string.Format("Invalid pins number. For second bowl must be in range [0; {0}]", 10 - lastBowlPinsCount));
+        }
+
+        // other behaviour here, e.g. last frame
+
+        if (pins == 10)
+        {
+            Action action;
+            
+            if (bowl == 19)
+            {
+                action = Action.RESET;
+            }
+            else if (bowl == 21)
+            {
+                action = Action.END_GAME;
+            }
+            else
+            {
+                action = Action.END_TURN;
+            }
+            
+            bowl += 2;
+            return action;
+        }
+
+        // if at the mid frame (or last frame)
+        if (bowl % 2 != 0)
+        {
+            Action action;
+            if (bowl == 21)
+            {
+                action = Action.END_GAME;
+            }
+            else
+            {
+                action = Action.TIDY;
+            }
+            
+            bowl++;
+            lastBowlPinsCount = pins;
+            return action;
+        }
+        else
+        {
+            Action action;
+            
+            if (bowl == 20 && pinsInGame - pins == 0)
+            {
+                action = Action.RESET;
+            }
+            else
+            {
+                action = Action.END_TURN;
+            }
+            
+            bowl++;
+            lastBowlPinsCount = 0;
+            return action;
         }
     }
-    
+
     public enum Action
     {
         TIDY,
