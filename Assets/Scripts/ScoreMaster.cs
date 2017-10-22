4395d14f05eec4de087480c92c707e1c6e92813b
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2017-10-22 16:53:54 +0300

    Added ScoreMaster

---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public static class ScoreMaster
+{
+    public static List<int> ScoreFrames(List<int> rolls)
+    {
+        List<int> frameList = new List<int>();
+        // todo implement
+        return frameList;
+    }
+}
